74ab87ea697590034156871eb5b97c8377e8c0f7
william widmer <williamwidmerr@gmail.com>
2016-01-26 22:03:48 -0500

    first commit

--- models.py
+++ models.py
@@ -3,30 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class portfolio(models.Model):
-    title = models.CharField(max_length=200)
-    short = models.CharField(max_length=200)
-    url = models.CharField(max_length=500, blank=True)
-    content = models.TextField()
-    introImage = models.ImageField()
-    sideBar = models.ImageField(blank=True)
+class TechnicalSkill(models.Model):
+    name = ''
+    years = 0
+    stars = 5
 
 
-class page(models.Model):
-    title = models.CharField(max_length=200)
-    descript = models.CharField(max_length=200)
-    content = models.TextField()
-    onMenu = models.BooleanField(default=False)
-
-
-class project(page):
-    description = models.CharField(max_length=500)
-    externalUrl = models.CharField(max_length=400)
-    introImage = models.CharField(max_length=200)
-
-class blog(page):
-    pub_date = models.DateTimeField("pub")
-
-class index_image(models.Model):
-    text = models.CharField(max_length=300, default=" ")
-    image = models.ImageField()
+class Experience(models.Model):
+    title = ''
+    company = ''
+    description = ''
+    achievements = ''
