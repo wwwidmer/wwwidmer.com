75863f2f08cb170b2689d0eb49b72fbf188a3803
william widmer <williamwidmerr@gmail.com>
2016-02-09 19:47:03 -0500

    model fields and admin to edit them

--- admin.py
+++ admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from wwwidmer.models import Experience, TechnicalSkill
 
 
+class technicalSkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stars')
+
+
+class experienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company')
+
+
+admin.site.register(TechnicalSkill, technicalSkillAdmin)
+admin.site.register(Experience, experienceAdmin)
 
--- models.py
+++ models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 
 class TechnicalSkill(models.Model):
-    name = ''
-    years = 0
-    stars = 5
+    name = models.CharField(max_length=100, primary_key=True)
+    stars = models.DecimalField(max_digits=2, decimal_places=1)
 
 
 class Experience(models.Model):
-    title = ''
-    company = ''
-    description = ''
+    company = models.CharField(max_length=100, primary_key=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100) 
     achievements = ''
